fece6c419d8b747287c8b57a4442e01bda73ec2c
trinhhuy <trinhhuyk57@gmail.com>
2017-04-12 13:17:30 +0700

    update supplier

--- resources/views/suppliers/products_suppliers.blade.php
+++ resources/views/suppliers/products_suppliers.blade.php
@@ -17,6 +17,118 @@
 <div class="page-content">
     <div class="row">
         <div class="col-xs-12">
+            <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Thêm mới</button>
+            <!-- Modal -->
+            <div class="modal fade" id="myModal" role="dialog">
+                <div class="modal-dialog">
+
+                    <!-- Modal content-->
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <button type="button" class="close" data-dismiss="modal">&times;</button>
+                            <h4 class="modal-title">Thêm mới nhà cung cấp</h4>
+                        </div>
+                        <div class="modal-body">
+                                <form class="form-horizontal" role="form" id="supplier_form" action="{{ url('suppliers/map-suppliers') }}" method="POST" enctype="multipart/form-data" >
+                                {!! csrf_field() !!}
+                                <input type="hidden" name="product_id" id="product_id" value="{{ $id }}"/>
+                                <div class="form-group">
+                                    <label class="col-sm-3 control-label no-padding-left">Nhà cung cấp</label>
+                                    <div class="col-sm-9">
+                                        <select name="supplier_id" class="form-control">
+                                            <option value="">-- Chọn nhà cung cấp --</option>
+                                            @foreach($suppliers as $key => $value)
+                                                <option value="{{  $value->id }}">{{  $value->name }}</option>
+                                            @endforeach
+                                        </select>
+                                        <p style="color:red;text-align: left;" id="supplier_id">{{$errors->first('supplier_id')}}</p>
+                                    </div>
+                                </div>
+
+                                <div class="form-group">
+                                    <label class="col-sm-3 control-label no-padding-left">Tình trạng</label>
+                                    <div class="col-sm-9">
+                                        <select name="state"  class="form-control">
+                                            <option value="">-- Chọn tình trạng --</option>
+                                            <option value="0">Hết hàng</option>
+                                            <option value="1">Còn hàng</option>
+                                            <option value="2">Đặt hàng</option>
+                                        </select>
+                                        <p style="color:red;text-align: left;" id="state">{{$errors->first('state')}}</p>
+                                    </div>
+                                </div>
+
+                                <div class="form-group">
+                                    <label class="col-sm-3 control-label no-padding-left">Giá nhập (có VAT)</label>
+                                    <div class="col-sm-9">
+                                        <input type="number" min = "0" class="form-control" name="import_price" placeholder="Nhập giá" >
+                                        <p style="color:red;text-align: left;" id="import_price">{{$errors->first('import_price')}}</p>
+                                    </div>
+                                </div>
+
+                                <div class="form-group">
+                                    <label class="col-sm-3 control-label no-padding-left">VAT</label>
+                                    <div class="col-sm-9">
+                                        <input type="number" min = "0" class="form-control" name="vat" placeholder="Nhập VAT" >
+                                        <p style="color:red;text-align: left;" id="vat">{{$errors->first('vat')}}</p>
+                                    </div>
+                                </div>
+
+                                <div class="form-group">
+                                    <label class="col-sm-3 control-label no-padding-left">Giá bán</label>
+                                    <div class="col-sm-9">
+                                        <input type="number" min = "0" class="form-control" name="saler_price" placeholder="Nhập giá" >
+                                        <p style="color:red;text-align: left;" id="saler_price">{{$errors->first('saler_price')}}</p>
+                                    </div>
+                                </div>
+
+                                <div class="form-group">
+                                    <label class="col-sm-3 control-label no-padding-left">Số lượng</label>
+                                    <div class="col-sm-9">
+                                        <input type="number" min = "0" class="form-control" name="quantity"  placeholder="Nhập số lượng" >
+                                        <p style="color:red;text-align: left;" id="quantity">{{$errors->first('quantity')}}</p>
+                                    </div>
+                                </div>
+
+                                <div class="form-group">
+                                    <label class="col-sm-3 control-label no-padding-left">Ảnh</label>
+                                    <div class="col-sm-9">
+                                        <input type="file" class="form-control" name="image"  >
+                                        <p style="color:red;text-align: left;" id="image">{{$errors->first('image')}}</p>
+                                    </div>
+
+                                </div>
+
+                                <div class="form-group">
+                                    <label class="col-sm-3 control-label no-padding-left">Mô tả</label>
+                                    <div class="col-sm-9">
+                                        <textarea class="form-control" name="description" id = "_description"></textarea>
+                                        <p style="color:red;text-align: left;" id="description">{{$errors->first('description')}}</p>
+                                    </div>
+
+                                </div>
+
+
+                                <div class="form-group">
+                                    <label class="col-sm-4 control-label no-padding-left"></label>
+                                    <button type="button" class="btn btn-success" id = "btn_save">
+                                        <i class="ace-icon fa fa-save bigger-110"></i>Lưu thông tin
+                                    </button>
+                                    <button type="button" class="btn btn-default" data-dismiss="modal">Hủy</button>
+                                </div>
+                            </form>
+
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <br>
+    <div class="row">
+        <div class="col-xs-12">
             <table id="dataTables-products" class="table table-striped table-bordered table-hover no-margin-bottom no-border-top">
                 <thead>
                     <tr>
@@ -43,94 +155,7 @@
             </table>
         </div>
     </div>
-    <br>
-    <div class="row">
-        <div class="col-xs-12">
-            @include('common.errors')
-            <form class="form-horizontal" role="form" id="product_form" action="{{ route('suppliers.store') }}" method="POST" enctype="multipart/form-data" >
-                {!! csrf_field() !!}
-                <input type="hidden" name="product_id" id="product_id" value="{{ $id }}"/>
-                <div class="form-group">
-                    <label class="col-sm-3 control-label no-padding-left">Nhà cung cấp</label>
-                    <div class="col-sm-6">
-                        <select name="supplier_id" id="supplier_id" class="form-control">
-                            <option value="">-- Chọn nhà cung cấp --</option>
-                            @foreach($suppliers as $key => $value)
-                                <option value="{{  $value->id }}">{{  $value->name }}</option>
-                            @endforeach
-                        </select>
-                    </div>
-                </div>
-
-                <div class="form-group">
-                    <label class="col-sm-3 control-label no-padding-left">Tình trạng</label>
-                    <div class="col-sm-6">
-                        <select name="state" id="state" class="form-control">
-                            <option value="">-- Chọn tình trạng --</option>
-                            <option value="0">Hết hàng</option>
-                            <option value="1">Còn hàng</option>
-                            <option value="2">Đặt hàng</option>
-                        </select>
-                    </div>
-                </div>
-
-                <div class="form-group">
-                    <label class="col-sm-3 control-label no-padding-left">Giá nhập (có VAT)</label>
-                    <div class="col-sm-6">
-                        <input type="number" class="form-control" name="import_price" id="import_price" placeholder="Nhập giá" >
-                    </div>
-                </div>
-
-                <div class="form-group">
-                    <label class="col-sm-3 control-label no-padding-left">VAT</label>
-                    <div class="col-sm-6">
-                        <input type="number" class="form-control" name="vat" id="vat" placeholder="Nhập VAT" >
-                    </div>
-                </div>
-
-                <div class="form-group">
-                    <label class="col-sm-3 control-label no-padding-left">Giá bán</label>
-                    <div class="col-sm-6">
-                        <input type="number" class="form-control" name="saler_price" id="import_price" placeholder="Nhập giá" >
-                    </div>
-                </div>
-
-                <div class="form-group">
-                    <label class="col-sm-3 control-label no-padding-left">Số lượng</label>
-                    <div class="col-sm-6">
-                        <input type="number" class="form-control" name="quantity" id="quantity" placeholder="Nhập số lượng" >
-                    </div>
-                </div>
-
-                <div class="form-group">
-                    <label class="col-sm-3 control-label no-padding-left">Ảnh</label>
-                    <div class="col-sm-6">
-                        <input type="file" class="form-control" name="image"  >
-                    </div>
-                </div>
-
-                <div class="form-group">
-                    <label class="col-sm-3 control-label no-padding-left">Mô tả</label>
-                    <div class="col-sm-6">
-                        <textarea class="form-control" name="description" id="description"></textarea>
-                    </div>
-                </div>
-
-
-                <div class="form-group">
-                    <label class="col-sm-4 control-label no-padding-left"></label>
-                    <button type="submit" class="btn btn-success">
-                        <i class="ace-icon fa fa-save bigger-110"></i>Lưu thông tin
-                    </button>
-                    <a onclick="cancel()" class="btn btn-danger">
-                        <i class="ace-icon fa fa-trash bigger-110"></i>Hủy
-                    </a>
-                </div>
-            </form>
-
-        </div>
     </div>
-
 </div><!-- /.page-content -->
 @endsection
 
@@ -144,10 +169,46 @@
     <script src="//cdn.ckeditor.com/4.5.7/standard/ckeditor.js"></script>
 <script>
 $(function () {
+    var datatable = $("#dataTables-products").DataTable({});
+    CKEDITOR.replace('_description');
+    $("#btn_save").on("click", function () {
+        for ( instance in CKEDITOR.instances )
+            CKEDITOR.instances[instance].updateElement();
+        var form = $('#supplier_form');
+        var data = new FormData(form[0]);
+        $.ajax({
+            headers: { 'X-CSRF-Token': $('input[name="_token"]').val() },
+            url: $('#supplier_form').attr('action'),
+            type: $('#supplier_form').attr('method'),
+            data: data,
+            processData: false,
+            cache:false,
+            contentType:false,
+            dataType: 'JSON',
+            success: function (res){
+                $("#supplier_id, #state, #import_price , #vat , #saler_price , #quantity, #image, #description").text('');
+                if(res.status == 'success'){
+                    $('#myModal').modal('hide');
+                    swal({
+                            title: "Tạo thành công",
+                            type: "success",
+                            showCancelButton: false,
+                            confirmButtonColor: "#DD6B55",
+                            confirmButtonText: "Ok",
+                            closeOnConfirm: false
+                        },
+                        function(){
+                            window.location.reload();
+                        });
+                } else {
+                    $.each(res.errors,function(index, value) {
+                        $("#"+index).text(value);
+                    });
+                }
 
-    var datatable = $("#dataTables-products").DataTable({
+            }
+        });
     });
-    CKEDITOR.replace('description');
 
     @include('scripts.click-datatable-delete-button')
 });

98fe72479a039fdbddeee7000066a20a7af14e93
Namvu <nam.vh@teko.vn>
2017-04-25 17:44:54 +0700

    Supplier Api

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ Route::get('manufacturers', 'ManufacturersController@index');
 Route::get('products', 'ProductsController@index');
 Route::get('products/{id}/detail', 'ProductsController@detail');
 Route::get('listProductSku', 'ProductsController@getListProductSku');
+Route::get('listSupplierByProductId', 'SuppliersController@getListSupplierByProductId');
 
 Route::group(['middleware' => 'auth:api'], function () {
     //

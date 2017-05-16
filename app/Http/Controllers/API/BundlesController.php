611e5ae584286bd85f2aebcd2822e63dc97d16f2
trinhhuy <trinhhuyk57@gmail.com>
2017-05-16 11:32:21 +0700

    update API bundle

--- app/Http/Controllers/API/BundlesController.php
+++ app/Http/Controllers/API/BundlesController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers\API;
 
+use DB;
 use App\Models\Bundle;
+use App\Models\Province;
+use App\Models\Product;
+use App\Models\SupplierSupportedProvince;
 use App\Http\Controllers\Controller;
 use App\Models\BundleProduct;
 use GuzzleHttp\Psr7\Request;
@@ -12,8 +16,43 @@ class BundlesController extends Controller
     public function getBundleProductByRegionId($regionId)
     {
         $bundleIds = Bundle::where('region_id',$regionId)->pluck('id');
-        $bundleProducts = BundleProduct::join('products','bundle_product.id_product', '=', 'products.id')
-            ->select('is_default','quantity','products.name as name','products.price as price')->get();
-        dd($bundleProducts);
+
+        $provinceIds = Province::where('region_id', $regionId)->pluck('id');
+
+        $bundleProductIds = BundleProduct::whereIn('id_bundle',$bundleIds)->pluck('id_product');
+
+        $supplierIds = SupplierSupportedProvince::whereIn('province_id', $provinceIds)
+            ->get()
+            ->pluck('supplier_id');
+
+        $products = Product::with('manufacturer', 'category')
+            ->select(DB::raw("`products`.`id`, `products`.`name` , `products`.`sku`, `product_supplier`.`image` as `source_url`, `products`.`manufacturer_id`, `products`.`category_id`, `product_supplier`.`quantity`"))
+            ->join('product_supplier', function ($q) use ($supplierIds) {
+                $q->on('product_supplier.product_id', '=', 'products.id')
+                    ->whereIn('product_supplier.supplier_id', $supplierIds)
+                    ->where('product_supplier.state', '=', 1);
+            })
+            ->whereIn('products.id',$bundleProductIds)->get();
+
+        $response = [];
+        foreach ($products as $product) {
+
+            $margin = MarginRegionCategory::where('category_id', $product->category_id)
+                ->where('region_id', $regionId)->first();
+
+            if ($margin) {
+                $productMargin = 1 + 0.01 * $margin->margin;
+            } else {
+                $productMargin = 1.05;
+            }
+
+            $product->best_price = ProductSupplier::where('product_id', $product->id)
+                ->whereIn('product_supplier.supplier_id', $supplierIds)
+                ->min(DB::raw('(if(product_supplier.price_recommend > 0, product_supplier.price_recommend, product_supplier.import_price * ' . $productMargin . '))'));
+
+            array_push($response,$product);
+        }
+
+        return $response;
     }
 }

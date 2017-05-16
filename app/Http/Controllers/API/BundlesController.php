5cc8a7ec0d18741c45d93fa273856e7ce9465f4c
trinhhuy <trinhhuyk57@gmail.com>
2017-05-16 13:44:07 +0700

    update code to commit

--- app/Http/Controllers/API/BundlesController.php
+++ app/Http/Controllers/API/BundlesController.php
@@ -4,39 +4,41 @@ namespace App\Http\Controllers\API;
 
 use DB;
 use App\Models\Bundle;
-use App\Models\Province;
 use App\Models\Product;
+use App\Models\Province;
+use App\Models\ProductSupplier;
+use App\Models\MarginRegionCategory;
 use App\Models\SupplierSupportedProvince;
 use App\Http\Controllers\Controller;
 use App\Models\BundleProduct;
-use GuzzleHttp\Psr7\Request;
 
 class BundlesController extends Controller
 {
-    public function getBundleProductByRegionId($regionId)
+    public function getBundleProductByRegionId($codeProvinceId)
     {
+        $regionId = Province::where('code', $codeProvinceId)->pluck('region_id');
+
         $bundleIds = Bundle::where('region_id',$regionId)->pluck('id');
 
         $provinceIds = Province::where('region_id', $regionId)->pluck('id');
-
-        $bundleProductIds = BundleProduct::whereIn('id_bundle',$bundleIds)->pluck('id_product');
 
         $supplierIds = SupplierSupportedProvince::whereIn('province_id', $provinceIds)
             ->get()
             ->pluck('supplier_id');
 
-        $products = Product::with('manufacturer', 'category')
+        $bundleProductIds = BundleProduct::whereIn('id_bundle',$bundleIds)->pluck('id_product');
+        $response = [];
+        foreach ($bundleProductIds as $id) {
+            dd($supplierIds);
+        $product = Product::with('manufacturer', 'category')
             ->select(DB::raw("`products`.`id`, `products`.`name` , `products`.`sku`, `product_supplier`.`image` as `source_url`, `products`.`manufacturer_id`, `products`.`category_id`, `product_supplier`.`quantity`"))
             ->join('product_supplier', function ($q) use ($supplierIds) {
                 $q->on('product_supplier.product_id', '=', 'products.id')
                     ->whereIn('product_supplier.supplier_id', $supplierIds)
                     ->where('product_supplier.state', '=', 1);
             })
-            ->whereIn('products.id',$bundleProductIds)->get();
-
-        $response = [];
-        foreach ($products as $product) {
-
+            ->findOrFail($id);
+        dd($product);
             $margin = MarginRegionCategory::where('category_id', $product->category_id)
                 ->where('region_id', $regionId)->first();
 
@@ -51,6 +53,7 @@ class BundlesController extends Controller
                 ->min(DB::raw('(if(product_supplier.price_recommend > 0, product_supplier.price_recommend, product_supplier.import_price * ' . $productMargin . '))'));
 
             array_push($response,$product);
+
         }
 
         return $response;

1ac9d25435ebb727001394f0a013a207157ca4ff
Namvu <nam.vh@teko.vn>
2017-05-18 09:46:20 +0700

    update

--- resources/views/attributes/datatables/action.blade.php
+++ resources/views/attributes/datatables/action.blade.php
@@ -4,6 +4,3 @@
 @if ($currentUser->hasAccess('attributes.edit'))
 <a class="green" href="{{ route('attributes.edit', $id) }}"><i class="ace-icon fa fa-pencil bigger-130"></i></a>
 @endif
-@if ($currentUser->hasAccess('attributes.destroy'))
-<a class="red" id="btn-delete-{{ $id }}" data-url="{{ route('attributes.destroy', $id) }}" href="javascript:;"><i class="ace-icon fa fa-trash-o bigger-130"></i></a>
-@endif

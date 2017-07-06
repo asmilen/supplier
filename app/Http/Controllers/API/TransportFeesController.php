6fb1681bebe25adc12a88dfc952ff06e79b893c5
Giang Thai Cuong <thaicuong.giang@gmail.com>
2017-07-06 14:38:52 +0700

    refactor

--- app/Http/Controllers/API/TransportFeesController.php
+++ app/Http/Controllers/API/TransportFeesController.php
@@ -2,10 +2,8 @@
 
 namespace App\Http\Controllers\API;
 
-use DB;
 use Validator;
 use App\Models\Province;
-use App\Models\TransportFee;
 use App\Http\Controllers\Controller;
 
 class TransportFeesController extends Controller
@@ -21,11 +19,6 @@ class TransportFeesController extends Controller
             return $validator->errors();
         }
 
-        return DB::table('provinces')
-            ->leftJoin('transport_fees', 'transport_fees.province_id', '=', 'provinces.id')
-            ->whereIn('provinces.code', request('province_codes'))
-            ->select(DB::raw('provinces.code as province_code, provinces.name as province_name, ifnull(transport_fees.percent_fee, 0) as shipping_fee'))
-            ->get()
-            ->keyBy('province_code');
+        return Province::getTransportFeesList();
     }
 }

b52d787ec4348198592712396250749cc853a151
NguyenHuuLam <lam.nh@teko.vn>
2017-07-06 09:53:07 +0700

    t :# This is a combination of 2 commits.
    fix filter supplier
    
    bundle js
    
    fix js
    
    fix conflic
    
    fix conflic1
    
    message queue
    
    fix

--- app/Models/MessageQueueLog.php
+++ app/Models/MessageQueueLog.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class MessageQueueLog extends Model
 {
-    //
+    protected $table = 'message_queue_logs';
+
+    public $timestamps = false;
+
+    protected $fillable = ['exchange', 'routingKey', 'body', 'created_at', 'updated_at'];
 }

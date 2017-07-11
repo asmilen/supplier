29a39b067f2ec40362752844aeadf5469d07f5d0
NguyenHuuLam <lam.nh@teko.vn>
2017-07-11 16:00:01 +0700

    fix models

--- app/Models/SalepriceLog.php
+++ app/Models/SalepriceLog.php
@@ -10,11 +10,12 @@ class SalepriceLog extends Model
     protected $table = 'saleprice_logs';
     protected $fillable = [
         'user_id',
-        'sku',
+        'product_id',
         'price',
         'store',
         'region',
         'status',
+        'detail',
         'created_at',
         'updated_at',
     ];

751eded42035ff66a940d7eb5d42c1566d6b1245
Giang Thai Cuong <thaicuong.giang@gmail.com>
2017-08-01 11:26:42 +0700

    update product supplier

---
+++ app/Http/Controllers/GoogleSpreadsheet/ProductSupplierPricesController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\GoogleSpreadsheet;
+
+use Sentinel;
+use App\Models\User;
+use App\Models\Supplier;
+use App\Models\ProductSupplier;
+use App\Http\Controllers\Controller;
+
+class ProductSupplierPricesController extends Controller
+{
+    public function update()
+    {
+        try {
+            $user = User::where('email', request('user_email'))->firstOrFail();
+
+            Sentinel::login($user);
+
+            $supplier = Supplier::where('name', request('supplier_name'))->firstOrFail();
+
+            foreach (request('products', []) as $productData) {
+                $productSupplier = ProductSupplier::where('supplier_id', $supplier->id)
+                    ->where('product_id', $productData['id'])
+                    ->first();
+
+                if (! $productSupplier) {
+                    continue;
+                }
+
+                $productSupplier->forceFill([
+                    'import_price' => request('import_price', 0),
+                    'min_quantity' => request('min_quantity', 0),
+                ])->save();
+            }
+
+            return response()->json([
+                'success' => true,
+            ]);
+        } catch (\Exception $e) {
+            return response()->json([
+                'error' => $e->getMessage(),
+            ], 422);
+        }
+    }
+}

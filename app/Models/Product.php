358e17150abd8aac2d74b1d46aeb8aa05659c0be
Giang Thai Cuong <thaicuong.giang@gmail.com>
2017-08-24 10:16:02 +0700

    update category margins to magento

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -80,6 +80,11 @@ class Product extends Model
         return $this->hasMany(Product::class, 'parent_id');
     }
 
+    public function productSuppliers()
+    {
+        return $this->hasMany(ProductSupplier::class);
+    }
+
     public function scopeActive($query)
     {
         return $query->where('status', true);

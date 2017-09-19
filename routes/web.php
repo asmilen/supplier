a29a839410d540c87d4061cb69909673a558545d
nam.vh@teko.vn <nam.vh@teko.vn>
2017-09-19 10:42:33 +0700

    add login teko

--- routes/web.php
+++ routes/web.php
@@ -6,10 +6,6 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('login', function () {
-    return redirect('/auth/google');
-});
-
 Route::get('auth/google', 'Auth\AuthController@redirectToProvider');
 Route::get('auth/google/callback', 'Auth\AuthController@handleProviderCallback');
 Route::get('auth/teko/callback', 'Auth\AuthController@handleTekoCallback');

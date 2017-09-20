d1d9ab3bcb9453b86261f65946041426fcd1ba4b
Giang Thai Cuong <thaicuong.giang@gmail.com>
2017-09-20 14:39:59 +0700

    refactor

--- app/Http/Middleware/SSOLogin.php
+++ app/Http/Middleware/SSOLogin.php
@@ -18,31 +18,52 @@ class SSOLogin
      */
     public function handle($request, Closure $next)
     {
-        if (isset($_COOKIE['_uat']) && !Sentinel::check())
-        {
-            $client = new Client([
-                'base_uri' => env('TEKO_ACC_URL'),
-            ]);
-
-            $response = $client->get('/api/validate_access_token?accessToken='.$_COOKIE['_uat']);
-
-            $userInfo = json_decode($response->getBody()->getContents(), true);
-
-            if (! isset($userInfo['error'])) {
-                $user = User::where('email', $userInfo['email'])->first();
-
-                if (! $user) {
-                    $user = User::forceCreate([
-                        'email' => $userInfo['email'],
-                        'name' => $userInfo['name'],
-                    ]);
-                }
-
-                Sentinel::login($user);
-
-            }
+        if ($this->shouldLoginViaTekoSSO()) {
+            $this->loginViaTekoSSO();
         }
 
         return $next($request);
+    }
+
+    protected function shouldLoginViaTekoSSO()
+    {
+        return isset($_COOKIE['_uat']) && Sentinel::guest();
+    }
+
+    protected function loginViaTekoSSO()
+    {
+        if (! $userInfo = $this->getUserInfoByTekoSSOCookie()) {
+            return false;
+        }
+
+        $user = User::where('email', $userInfo['email'])->first();
+
+        if (! $user) {
+            $user = User::forceCreate([
+                'email' => $userInfo['email'],
+                'name' => $userInfo['name'],
+            ]);
+        }
+
+        Sentinel::login($user);
+
+        return true;
+    }
+
+    protected function getUserInfoByTekoSSOCookie()
+    {
+        $client = new Client([
+            'base_uri' => env('TEKO_ACC_URL'),
+        ]);
+
+        $response = $client->get('/api/validate_access_token?accessToken='.$_COOKIE['_uat']);
+
+        $userInfo = json_decode($response->getBody()->getContents(), true);
+
+        if (isset($userInfo['error']) || empty($userInfo['email'])) {
+            return false;
+        }
+
+        return $userInfo;
     }
 }

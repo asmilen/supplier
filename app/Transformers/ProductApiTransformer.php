9dd72176b3562a5e5f9cf8ccc08a3d7aeaab82fc
NguyenHuuLam <lam.nh@teko.vn>
2017-09-21 11:46:44 +0700

    fix price

--- app/Transformers/ProductApiTransformer.php
+++ app/Transformers/ProductApiTransformer.php
@@ -14,10 +14,12 @@ use Illuminate\Support\Facades\DB;
 class ProductApiTransformer extends TransformerAbstract
 {
 
-    public function __construct($provinceIds, $code)
+    public function __construct($provinceIds, $code, $provinceFee, $feeValue)
     {
         $this->provinceIds = $provinceIds;
         $this->code = $code;
+        $this->provinceFee = $provinceFee;
+        $this->feeValue = $feeValue;
     }
 
     /**
@@ -27,27 +29,17 @@ class ProductApiTransformer extends TransformerAbstract
     public function transform($data)
     {
         $regions = Province::whereIn('code', request('province_ids'))->pluck('region_id'); // tìm miền của tỉnh mua hàng
-        
-        $product = Product::with('manufacturer', 'category')
-            ->select(DB::raw("`products`.`id`, `products`.`name` , `products`.`sku`, `products`.`image` as `source_url`, `products`.`manufacturer_id`, `products`.`category_id`, `product_supplier`.`quantity`"))
-            ->leftJoin('product_supplier', function ($q) use ($regions) {
-                $q->on('product_supplier.product_id', '=', 'products.id')
-                    ->where('product_supplier.region_id', $regions[0])
-                    ->where('product_supplier.state', '=', 1);
-            })
-            ->findOrFail($data['id']);// kiểm tra thông tin sản phẩm cần mua
 
-        $margin = MarginRegionCategory::where('category_id', $product->category_id)
+        $margin = MarginRegionCategory::where('category_id', $data['category_id'])
             ->whereIn('region_id', $regions)->first(); // tính margin của category sản phẩm
 
         $marginValue = ($margin ? 1 + 0.01 * $margin->margin : 1.05); // giá trị của margin
 
         $province = Province::whereIn('code', request('province_ids'))->pluck('id'); // tìm tỉnh mua hàng
 
-        $provinceFee = TransportFee::where('province_id', $province ? $province[0] : 0)->first();// phí ship của tỉnh mua hàng
+        $provinceFee = $this->provinceFee;// phí ship của tỉnh mua hàng
 
-        $feeValue = ($provinceFee ? $provinceFee->percent_fee : 0) * 0.01; //giá trị của phí ship của tỉnh mua hàng
-
+        $feeValue = $this->feeValue; //giá trị của phí ship của tỉnh mua hàng
 
         $minPrice = ProductSupplier::where('product_id', $data['id'])
             ->where('product_supplier.region_id',  $regions[0])
@@ -74,38 +66,23 @@ class ProductApiTransformer extends TransformerAbstract
         }
         $w_margin = ($margin ? $margin->margin : 5) * 0.01;
 
-        $product->best_price = ProductSupplier::where('product_id', $data['id'])
+        $product = ProductSupplier::select('product_supplier.import_price')
+            ->where('product_id', $data['id'])
             ->where('product_supplier.region_id', $regions[0])
             ->where('product_supplier.state', '=', 1)
             ->where('product_supplier.supplier_id',  $minPrice->supplier->id)
-            ->min(DB::raw('(if(product_supplier.price_recommend > 0, product_supplier.price_recommend, ceil(product_supplier.import_price * ' . $productFee . '/1000) * 1000))'));
+            ->first();
 
-        $product->import_price = ProductSupplier::where('product_id', $data['id'])
-            ->where('product_supplier.region_id', $regions[0])
-            ->where('product_supplier.state', '=', 1)
-            ->where('product_supplier.supplier_id',  $minPrice->supplier->id)
-            ->min(DB::raw('ceil(product_supplier.import_price * (' . $productFee . '-' . $w_margin . ')/1000) * 1000'));
-
-        $product->import_price_w_margin = ProductSupplier::where('product_id', $data['id'])
-            ->where('product_supplier.region_id', $regions[0])
-            ->where('product_supplier.state', '=', 1)
-            ->where('product_supplier.supplier_id',  $minPrice->supplier->id)
-            ->min(DB::raw('ceil(product_supplier.import_price * ' . $productFee . '/1000) * 1000'));
-
-        $product->recommended_price = ProductSupplier::where('product_id', $data['id'])
-            ->where('product_supplier.region_id', $regions[0])
-            ->where('product_supplier.state', '=', 1)
-            ->where('product_supplier.supplier_id',  $minPrice->supplier->id)
-            ->where('price_recommend', $product->best_price)
-            ->min('product_supplier.price_recommend');
+        $import_price = ceil(rtrim(rtrim(sprintf('%f', $product->import_price * ($productFee - $w_margin) / 1000), '0'), '.')) * 1000;
+        $import_price_w_margin = ceil(rtrim(rtrim(sprintf('%f', $product->import_price * $productFee / 1000), '0'), '.')) * 1000;
 
         return [
             'id' => $data['id'],
             'name' => html_entity_decode($data['name']),
-            'price' => $product->best_price,
-            'import_price' => $product->import_price,
-            'import_price_w_margin' => $product->import_price_w_margin,
-            'recommended_price' => $product->recommended_price ? $product->recommended_price : 0,
+            'price' => (Int)$data['price'],
+            'import_price' => $import_price,
+            'import_price_w_margin' => $import_price_w_margin,
+            'recommended_price' =>  $data['recommended_price'] ? (Int)$data['recommended_price'] : 0,
             'source_url' => $data['image'],
             'sku' => $data['sku']
         ];

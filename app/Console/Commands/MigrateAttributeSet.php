230cebfff30d245df3a9b23c0a3f7d283e0248b4
Admin <admin@Admins-MacBook-Air.local>
2017-11-01 00:25:15 +0700

    update

--- app/Console/Commands/MigrateAttributeSet.php
+++ app/Console/Commands/MigrateAttributeSet.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use App\Models\Attribute;
+use App\Models\Category;
 use Illuminate\Console\Command;
 use DB;
 
@@ -39,19 +40,68 @@ class MigrateAttributeSet extends Command
      */
     public function handle()
     {
+        $this->line('Deleting Attribute and Category Attribute Table');
+
         DB::table('attributes')->delete();
+        DB::table('attribute_category')->delete();
+
+        $this->line('Deleted Attribute and Category Attribute Table');
+
+        $this->line('Migrating Attributes');
+
+        $supplierBackendTypes = ['int','decimal','varchar','text'];
+        $supplierFrontendInput = ['select','multiselect','text','textarea'];
 
         $magentoAttributes = DB::connection('magento')->table('eav_attribute')->get();
 
         foreach ($magentoAttributes as $magentoAttribute)
         {
-            $attribute = Attribute::forceCreate([
+             Attribute::forceCreate([
                 'slug' => $magentoAttribute->attribute_code,
-                'backend_type' => $magentoAttribute->backend_type,
-                'frontend_input' => $magentoAttribute->frontend_input ? $magentoAttribute->frontend_input : '',
+                'backend_type' => in_array($magentoAttribute->backend_type,$supplierBackendTypes) ? $magentoAttribute->backend_type : 'varchar',
+                'frontend_input' => in_array($magentoAttribute->frontend_input,$supplierFrontendInput) ? $magentoAttribute->frontend_input : 'text',
                 'name' => $magentoAttribute->frontend_label ? $magentoAttribute->frontend_label : '',
                 'id' => $magentoAttribute->attribute_id,
             ]);
         }
+
+        $this->line('Migrated Attributes');
+
+        $this->line('Migrating Category Attribute');
+
+        $magentoAttributeSets = DB::connection('magento')->table('eav_attribute_sets')->get();
+
+        foreach($magentoAttributeSets as $magentoAttributeSet)
+        {
+            $category = Category::where('magento_attribute_set_id',$magentoAttributeSet->attribute_set_id)->first();
+            $category->attributes()->save($magentoAttributeSet->attribute_id);
+        }
+
+        $this->line('Migrated Category Attribute');
+
+        $this->line('Migrating Product Attribute');
+
+        $magentoBackendTypes = ['int','decimal','varchar','text','datetime'];
+
+        $success = 0 ; $errors = 0;
+
+        foreach ($magentoBackendTypes as $magentoBackendType)
+        {
+            $productAttributes = DB::connection('magento')->table('catalog_product_entity_' . $magentoBackendType)->get();
+            foreach ($productAttributes as $productAttribute)
+            {
+                try {
+                    $product = Product::where('sku',$productAttributes->sku)->first();
+                    $attribute = Attribute::find($productAttribute->attribute_id);
+                    $product->updateAttribute($attribute,$productAttribute->value);
+                    $success++;
+                } catch (\Exception $ex) {
+                    \Log::error($ex);
+                    $errors++;
+                }
+            }
+        }
+
+        $this->line('Migrated Product Attribute: ' . $success . ' success ' . $errors . ' errors');
     }
 }
